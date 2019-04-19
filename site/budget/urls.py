670bb84a8b7f41932d792b8c36ea04f68179af98
Jack Stringer <jackstringer@Jacks-MacBook-Pro.local>
2019-04-19 14:30:32 -0700

    Cleaned up file structure

--- site/budget/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'budget'
-urlpatterns = [
-    path('api/accounts', views.AccountList.as_view(), name='accounts'),
-    path('api/transactions', views.TransactionList.as_view(), name='transactions')
-]
